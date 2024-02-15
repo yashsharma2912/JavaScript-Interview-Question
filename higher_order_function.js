9ff0b3d1172dd3ffb1d467371440b14717f25cc1
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-02-15 20:08:31 +0530

    Create higher_order_function.js

---
+++ higher_order_function.js
@@ -0,0 +1,19 @@
+// A function which takes function as a parameter
+
+function salary(number){
+    return 5*number;
+}
+
+  setTimeout (salary,3000)
+
+const mySal = [50,50,80,60];
+
+mySal.filter(n=> n > 50);
+console.log(mySal);
+function funct(){
+    return 25+25;
+}
+() => {}
+(funct) => {}
+(funct) => () => {}
+(funct) => async() => {}
