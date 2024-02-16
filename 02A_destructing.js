b8398d802af6b38ae7d5c37286b67f9729942e61
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-02-16 09:41:34 +0530

    Update 02A_destructing.js

--- 02A_destructing.js
+++ 02A_destructing.js
@@ -13,3 +13,30 @@ const b = 32;
 [a,b] = [b,a]
 console.log(a);
 console.log(b);
+
+const colors = [ "red", "green","pink","white"];
+[colors[0],colors[3]] =[colors[3],colors[0]]
+
+// use of rest operator it will assign remaining colors to remains element
+const[first, second, ...remains] = colors;
+
+// EXTRACT VALUES FROM AN OBJECT
+
+function displayPerson({name,age,city}){
+ console.log(`name: ${name}`)
+ console.log(`age: ${age}`)
+ console.log(`city: ${city}`)
+}
+
+const person1 = {
+    name: " john",
+    age: 21,
+    city: "oosmanabad",
+}
+const person2 = {
+    name: "smith",
+    age: 32,
+    city: "sangali",
+}
+
+displayPerson(person1);
