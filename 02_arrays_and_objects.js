10c673c6e95526ec5ed9f072faca460f7af5adc7
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-02-16 09:06:47 +0530

    arr and obj

---
+++ 02_arrays_and_objects.js
@@ -0,0 +1,16 @@
+// creation of an array 
+let arr = [12,22,35,12,"yash"]
+arr.push(20);
+console.log(arr);
+
+//creation of an object
+// store element in key value pair
+let obj = {
+    Name: "Yash",
+    Surname: "Sharma",
+    // guess the output of salary
+    Salary: "₹" + 10 + 20,
+}
+// Accessing of an element in object
+console.log(obj.Name)
+console.log(obj)
