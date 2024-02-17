614cdca69ea8c32d02dff200d1e00738101a5f9d
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-02-17 10:34:48 +0530

    Create 04_constructor.js

---
+++ 04_constructor.js
@@ -0,0 +1,13 @@
+// a fuction which can be created for 
+// making an instances of a product
+
+function card(img, title, btn){
+    this.img = "url";
+    this.title = "cheap crad";
+    this.btn = false;    
+}
+
+// instances of above card
+const crad1 = new card("rul","rich cards",true)
+const crad2 = new card("pql","vintage cards",true)
+const crad3 = new card("ras","super cards",false)
